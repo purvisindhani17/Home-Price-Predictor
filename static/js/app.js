cea08fa9d2c19d23fdf337b7627f3fffc8a7d3f6
purvisindhani17 <purvisindhani17@gmail.com>
2025-12-19 21:16:33 +0530

    Refactor bathroom and BHK value functions and update URLs
    Updated functions to return index directly and replaced hardcoded API URLs with relative paths.

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,26 @@
+
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
   for(var i in uiBathrooms) {
     if(uiBathrooms[i].checked) {
-        return parseInt(i)+1;
+        return i+1;
     }
   }
   return -1; // Invalid Value
 }
 
+
+
 function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
   for(var i in uiBHK) {
     if(uiBHK[i].checked) {
-        return parseInt(i)+1;
+        return i+1;
     }
   }
   return -1; // Invalid Value
 }
+
 
 function onClickedEstimatePrice() {
   console.log("Estimate price button clicked");
@@ -26,8 +30,8 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+   // FIX: Replaced hardcoded URL with the relative path.
+   var url = "/predict_home_price"; 
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -43,10 +47,13 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log( "document loaded" );
-  // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+      
+  // FIX: Replaced hardcoded URL with the relative path.
+  var url = "/get_location_names"; 
+  
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
+      console.log(data)
       if(data) {
           var locations = data.locations;
           var uiLocations = document.getElementById("uiLocations");
