d4904d9851fa3ffa73f9aae131777c084f712ab2
purvisindhani17 <purvisindhani17@gmail.com>
2025-12-19 21:06:13 +0530

    Fix return value parsing and update URL handling

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
   for(var i in uiBathrooms) {
     if(uiBathrooms[i].checked) {
-        return i+1;
+        return parseInt(i)+1;
     }
   }
   return -1; // Invalid Value
@@ -12,7 +12,7 @@ function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
   for(var i in uiBHK) {
     if(uiBHK[i].checked) {
-        return i+1;
+        return parseInt(i)+1;
     }
   }
   return -1; // Invalid Value
@@ -26,8 +26,8 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-    var url = "/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-    //  var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+  var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -43,12 +43,10 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log( "document loaded" );
-      var url = "/get_location_names"; //Use this if you are NOT using nginx which is first 7 tutorials
-
-    // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards  
+  // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+  var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
-      console.log(data)
       if(data) {
           var locations = data.locations;
           var uiLocations = document.getElementById("uiLocations");
